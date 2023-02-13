c9d9d2853c0350f098a297cbe751d585a939a54a
Andrea.Nathansen <andrea.nathansen@student.hpi.de>
2023-02-13 18:21:57 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='protein-prompt-tuning',
+      version='1.0',
+      description='Prompt tuning toolkit for the protein language models ProtGPT2 and RITA',
+      url='https://github.com/AndreaNathansen/protein-prompt-tuning',
+      author='Original mkultra implementation: Corolla Johnson. Adaptation to protein language models: Andrea Nathansen & Kevin Klein',
+      author_email='no_email_given@gmail.com',
+      license='MIT',
+      packages=['mkultra'])
