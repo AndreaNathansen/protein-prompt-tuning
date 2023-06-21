6ac08349a7f7d7a8a246d0e10975ff4ae7aa8332
andrea.nathansen <andrea.nathansen@student.hpi.de>
2023-06-21 18:11:06 +0200

    Add evaluation scripts for finetuned models

---
+++ RITA_finetuned_comparison_trainvaltest.py
@@ -0,0 +1,81 @@
+"""
+Perplexity evaluation for a prompt-tuned RITA model on the training and validation set for a training run
+and on the test set. The prompt-tuned model along with the datasets can be
+loaded by specifying the config (see folder training_configs/) that was used for training the prompt.
+"""
+
+import argparse
+import csv
+import json
+import os
+from pathlib import Path
+
+import mkultra.sequence_loader as sequence_loader
+import numpy as np
+import torch
+from mkultra.evaluator import Evaluator
+from mkultra.tuning import RITAPromptTuningLM
+from torch.utils.data import DataLoader
+from utils.train_utils import seed_everything
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, PretrainedConfig
+
+parser = argparse.ArgumentParser(prog="Prompt Tuning")
+parser.add_argument("--config", dest="config", help="path to the JSON config file", required=True)
+args = parser.parse_args()
+
+with open(args.config) as config_file:
+    config = json.load(config_file)
+
+seed = config["seed"]
+seed_everything(seed)
+model_name = Path(config['model']).stem
+
+# Checkpoint loading
+model = AutoModelForCausalLM.from_config(PretrainedConfig.from_json_file(f"../transformers/{model_name}_finetune_test/config.json"), trust_remote_code=True).half().to("cuda")
+model.load_state_dict(torch.load(f"../transformers/{model_name}_finetune_test/pytorch_model.bin"))
+model.eval()
+
+csv_path = os.path.join("experiment_results", f"finetuned-{model_name}-trainvaltest_comparison.csv") 
+header = ["train", "val", "test"]
+
+block_size = config["block_size"]-config["n_tokens"]
+
+
+torch.cuda.empty_cache()
+tokenizer = AutoTokenizer.from_pretrained(config["model"])
+
+dataset_train = sequence_loader.FastaDataset(config["dataset_file_train"], tokenizer, block_size, tokenizer.vocab['<PAD>'], tokenizer.vocab['<EOS>'], tokenizer.vocab['<EOS>'])
+dataloader_train = DataLoader(dataset_train, batch_size=config["batch_size"], shuffle=False)
+dataset_val = sequence_loader.FastaDataset(config["dataset_file_validation"], tokenizer, block_size, tokenizer.vocab['<PAD>'], tokenizer.vocab['<EOS>'], tokenizer.vocab['<EOS>'])
+dataloader_val = DataLoader(dataset_val, batch_size=config["batch_size"], shuffle=False)
+dataset_test = sequence_loader.FastaDataset(config["dataset_file_test"], tokenizer, block_size, tokenizer.vocab['<PAD>'], tokenizer.vocab['<EOS>'], tokenizer.vocab['<EOS>'])
+dataloader_test = DataLoader(dataset_test, batch_size=config["batch_size"], shuffle=False)
+
+# One entry {train: ..., val: ..., test: ...}
+seed_everything(seed)
+torch.cuda.empty_cache()
+
+perplexities = {}
+
+perplexities["train"] = Evaluator(
+    model=model,
+    is_prompt_tuned=False,
+    data_loader_test=dataloader_train).evaluate_perplexity()
+
+perplexities["val"] = Evaluator(
+    model=model,
+    is_prompt_tuned=False,
+    data_loader_test=dataloader_val).evaluate_perplexity()
+
+perplexities["test"] = Evaluator(
+    model=model,
+    is_prompt_tuned=False,
+    data_loader_test=dataloader_test).evaluate_perplexity()
+
+torch.cuda.empty_cache()
+
+with open(csv_path, 'w') as f:
+    csvwriter = csv.DictWriter(f, fieldnames=header)
+    csvwriter.writeheader()
+    csvwriter.writerow(perplexities)
---
+++ RITA_finetuned_sequence_generation.py
@@ -0,0 +1,77 @@
+import argparse
+import csv
+import json
+import os
+from pathlib import Path
+
+"""
+Script for generating a number of sequences with a prompt-tuned RITA model and the respective
+base model (= without prompt). The prompt-tuned model along with its base model can be
+loaded by specifying the config (see folder training_configs/) that was used for training the prompt.
+Currently, sequences are generated from scratch without any starting amino acid.
+Also, currently generates 193 sequences (size of our main test set) in steps of 10. TODO: make this flexible.
+The generation sampling parameters are taken from https://github.com/lightonai/RITA
+"""
+
+import numpy as np
+import torch
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from mkultra.checkpoint_loader import CheckpointLoader
+from mkultra.tuning import RITAPromptTuningLM
+from utils.train_utils import seed_everything
+
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          PretrainedConfig, pipeline)
+
+parser = argparse.ArgumentParser(prog="Prompt Tuning")
+parser.add_argument("--config", dest="config", help="path to the JSON config file", required=True)
+parser.add_argument("--num-sequences", dest="num_sequences", type=int, help="number of sequences to generate", required=True)
+parser.add_argument("--batch-size", dest="batch_size", type=int, help="amount of sequences to generate at once", required=True)
+args = parser.parse_args()
+
+with open(args.config) as config_file:
+    config = json.load(config_file)
+
+seed = config["seed"]
+seed_everything(seed)
+model_name = Path(config['model']).stem
+
+block_size = config["block_size"]-config["n_tokens"]
+num_sequences = args.num_sequences
+batch_size = args.batch_size
+num_batches = int(np.ceil(num_sequences / batch_size))
+last_batch_size = num_sequences % batch_size
+
+torch.cuda.empty_cache()
+tokenizer = AutoTokenizer.from_pretrained(config["model"])
+
+# # Generate sequences with the base model
+# seed_everything(seed)
+# Checkpoint loading
+model = AutoModelForCausalLM.from_config(PretrainedConfig.from_json_file(f"../transformers/{model_name}_finetune_test/config.json"), trust_remote_code=True).half().to("cuda")
+model.load_state_dict(torch.load(f"../transformers/{model_name}_finetune_test/pytorch_model.bin"))
+model.eval()
+
+for n in range(num_batches):
+    if n == num_batches - 1:
+        num_seq = last_batch_size
+    else:
+        num_seq = batch_size
+    # leave out the EOS token that the RITA tokenizer always appends
+    input_ids = tokenizer("<EOS>", return_tensors="pt").input_ids[:, :-1].to("cuda")
+    output = model.generate(input_ids=input_ids, max_length=block_size, do_sample=True, top_k=950, repetition_penalty=1.2, 
+                        num_return_sequences=num_seq, eos_token_id=2)
+    sequences = [tokenizer.decode(output_ids) for output_ids in output]                   
+    sequences_records = [SeqRecord(seq=Seq(sequence.replace('<EOS>','').replace(' ', '')), id=str(n*batch_size+j), name=f'generated_prot_{j}') for j, sequence in enumerate(sequences)]
+    with open(os.path.join("generated_sequences", f"finetuned-{model_name}-generated.fasta"), 'a') as f:
+        SeqIO.write(sequences_records, f, "fasta")
+
+    del input_ids
+    del output
+    del sequences
+    del sequences_records
+    torch.cuda.empty_cache()
+del model
+torch.cuda.empty_cache()
